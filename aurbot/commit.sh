6fb3751853552df609d49818b5c160ac52078043
Ken <github@kenh.fr>
2020-01-20 23:41:42 +0100

    added commit script

---
+++ aurbot/commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+REPONAME=ken-aur.db.tar.gz
+for f in /home/build/repo/"$1"*; do
+    [ -e "$f" ] && rm "$f" && repo-remove /home/build/repo/$REPONAME "$1" \
+    && echo "Removed old package $f"
+    break
+done
+
+for f in "$1"*.pkg.tar.xz; do
+    [ -e "$f" ] && cp "$f" /home/build/repo/ && repo-add /home/build/repo/$REPONAME /home/build/repo/"$f" \
+    && echo "Added new package $f"
+    break
+done
